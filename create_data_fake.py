e5392880ba1c75a3264e346033c63d8a3927478a
HeinerAlejandro <heiner.enis@gmail.com>
2023-03-27 23:51:53 -0300

    feat: Add script to create data fake

---
+++ create_data_fake.py
@@ -0,0 +1,33 @@
+import os
+
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ieb_product_updater.settings")
+django.setup()
+
+import decimal
+
+from faker import Faker
+from model_bakery.recipe import Recipe
+
+from core.models import Product
+
+
+fake = Faker()
+
+for k in range(100):
+
+    base_price = fake.pydecimal(
+        left_digits=5,
+        right_digits=2,
+        positive=True,
+        min_value=1000.00
+    )
+
+    product = Recipe(
+        Product,
+        description=fake.name(),
+        buying_price=base_price,
+        selling_price=base_price * decimal.Decimal(1.3)
+    )
+
+    product.make()
